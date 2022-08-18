43763b575cadb9f1a677a63cea95c511bc9067b3
Thierry <varelathierry@gmail.com>
2022-08-18 16:16:01 -0300

    requisito 10 renderizando a lista de missões

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 export default class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions"><Title headline="Missões" /></div>
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {missions
+          .map(({ name, year, country, destination }) => (<MissionCard
+            name={ name }
+            year={ year }
+            country={ country }
+            destination={ destination }
+            key={ name }
+          />))}
+      </div>
     );
   }
 }
