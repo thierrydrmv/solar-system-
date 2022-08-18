bb098c8ccdb85af88fcff9ffd9366ea668164844
Thierry <varelathierry@gmail.com>
2022-08-18 15:25:40 -0300

    requisito 8 reutilizando o componente title

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 export default class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions"> </div>
+      <div data-testid="missions"><Title headline="Missões" /></div>
     );
   }
 }
