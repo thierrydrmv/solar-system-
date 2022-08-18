90bff2dd8bcc681ebdff0d445d7e11f3cdd1e95e
Thierry <varelathierry@gmail.com>
2022-08-18 15:20:24 -0300

    requisito 6 renderizando lista de planetas

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 export default class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets
+          .map(({ name, image }) => (<PlanetCard
+            planetName={ name }
+            planetImage={ image }
+            key={ name }
+          />))}
       </div>
     );
   }
